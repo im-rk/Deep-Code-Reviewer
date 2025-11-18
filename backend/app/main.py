8b3275a27f009f55b4c3340bf6540bd735ae8835
im-rk <krramkumar2006g@gmail.com>
2025-11-18 20:35:48 +0530

    conveted the string from response.content to a json format

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI,Request
 from app.model_loader import LLMclient
 from app.review_service import review_code
 from pydantic import BaseModel
-
+import json
 # Define request schema
 class CodeRequest(BaseModel):
     code: str
@@ -20,5 +20,15 @@ async def review(request:CodeRequest):
     print("Received code:", code[:100])  # first 100 chars
     print("Starting generation...")
     result=review_code(code,llm,language,file_name)
-    print(result)
-    return result
+    result=result[7:]
+    result=result[:-3]
+    result=result.strip()
+
+    try:
+        result_dict=json.loads(result)
+        print(f"Parsed dictionary type: {type(result_dict)}")
+        print(f"Parsed dictionary content: {result_dict}")
+        return result_dict
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+        return {"error": "Failed to parse JSON from LLM response", "details": str(e)}
